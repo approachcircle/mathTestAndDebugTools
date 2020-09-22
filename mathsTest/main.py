24c8fd61453ab365da6192eebe01558f74344f4d
Oscar McDermott <osc-mcd@protonmail.com>
2020-09-22 23:28:33 +0100

    added new question, added comments etc

--- mathsTest/main.py
+++ mathsTest/main.py
@@ -6,13 +6,8 @@ import tkinter
 
 # yes, i put this all in a 'try' block
 try:
-    q1 = "" # define all the vars at the start cause why not
-    q2 = ""
-    q3 = ""
-    q4 = ""
-    q5 = ""
-    menu = ""
-    debugAns = ""
+    q1 = q2 = q3 = q4 = q5 = q6 = q7 = q8 = q9 = q10 = q11 = q12 = q13 = q14 = q15 = q16 = q17 = q18 = q19 = q20 = "" # question number variables
+    menu = debugAns = "" # other variables
     print("LOAD OK") # start menu
     print("Enter \"test\" or \"debug\".")
     print("debug- Shows debugging utilities availible")
@@ -26,10 +21,20 @@ try:
         q1 = input("Answer:")
         if q1 == "12":
             print("Correct, the answer is 12.")
-            input()
+            input("Press enter to continue:")
         else:
             print("Incorrect, the answer is 12.")
-            input()
+            input("Press enter to continue:")
+        os.system('cls')
+        print("Question 2/5")
+        print("3.14 + 2.42")
+        q2 = input("Answer:")
+        if q2 == "5.56":
+            print("Correct, the answer is 5.56.")
+            input("Press enter to continue:")
+        else:
+            print("Incorrect, the answer is 5.56.")
+            input("Press enter to continue:")
     elif menu == "debug": # elif the user enters "debug", then...
         os.system('cls') # clear onscreen text
         print("DEBUG OK") # begin debug
@@ -67,8 +72,8 @@ try:
                 turtle.forward(50) # AAAAAAAAAAAAAAAAAAAAAAA
                 turtle.left(100) # AAAAAAAAAAAAAAAAAAAAAAA
 except Exception: # this catches any exceptions thrown
-    input("oops, an exception occured :/ press enter for debug info.") # friendly exception notice
+    input("oops, an exception occured :/ press enter for debug info...") # friendly exception notice
     traceback.print_exc() # print the exception
-    input("Press enter to continue.")
+    input("Press enter to continue:")
 print("-END-")
-input("Press enter to terminate python job.")
+input("Press enter to terminate python job")
